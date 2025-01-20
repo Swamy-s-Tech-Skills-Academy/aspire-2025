26ae8728c12fb7d8d6f2a5b9d4676b8e2ed56e25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-20 12:36:08 +0530

    Canges

--- src/AspireStarterProjectSSO/AspireApp/AspireApp.AppHost/Program.cs
+++ src/AspireStarterProjectSSO/AspireApp/AspireApp.AppHost/Program.cs
@@ -1,12 +1,45 @@
+const string WEBAPP_HTTP_ENVIRONMENT_VARIABLE = "WEBAPP_HTTP";
+const string IDP_HTTP_ENVIRONMENT_VARIABLE = "IDP_HTTP";
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
+var identityProvider = builder.AddProject<Projects.OpenIdConnectProvider>("identityprovider")
+    .WithExternalHttpEndpoints();
+
 var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice");
 
-builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+var webFrontend = builder.AddProject<Projects.AspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WithReference(apiService);
+
+var webAppHttp = webFrontend.GetEndpoint("http");
+var webAppHttps = webFrontend.GetEndpoint("https");
+
+if (webAppHttps.Exists)
+{
+    identityProvider.WithEnvironment(WEBAPP_HTTP_ENVIRONMENT_VARIABLE, () => $"{webAppHttps.Scheme}://{webAppHttps.Host}:{webAppHttps.Port}");
+}
+else
+{
+    identityProvider.WithEnvironment(WEBAPP_HTTP_ENVIRONMENT_VARIABLE, () => $"{webAppHttp.Scheme}://{webAppHttp.Host}:{webAppHttp.Port}");
+}
+
+var idpAppHttp = identityProvider.GetEndpoint("http");
+var idpAppHttps = identityProvider.GetEndpoint("https");
+
+if (idpAppHttps.Exists)
+{
+    webFrontend.WithEnvironment(IDP_HTTP_ENVIRONMENT_VARIABLE, () => $"{idpAppHttps.Scheme}://{idpAppHttps.Host}:{idpAppHttps.Port}");
+    apiService.WithEnvironment(IDP_HTTP_ENVIRONMENT_VARIABLE, () => $"{idpAppHttps.Scheme}://{idpAppHttps.Host}:{idpAppHttps.Port}");
+}
+else
+{
+    webFrontend.WithEnvironment(IDP_HTTP_ENVIRONMENT_VARIABLE, () => $"{idpAppHttp.Scheme}://{idpAppHttp.Host}:{idpAppHttp.Port}");
+    apiService.WithEnvironment(IDP_HTTP_ENVIRONMENT_VARIABLE, () => $"{idpAppHttp.Scheme}://{idpAppHttp.Host}:{idpAppHttp.Port}");
+}
+
 
 builder.Build().Run();
