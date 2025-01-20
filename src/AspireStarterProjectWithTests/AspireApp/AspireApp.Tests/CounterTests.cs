b6da167493feb795fdd1c2e96bf30360dbd9e98a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-20 11:49:41 +0530

    Added Counter Tests

---
+++ src/AspireStarterProjectWithTests/AspireApp/AspireApp.Tests/CounterTests.cs
@@ -0,0 +1,30 @@
+﻿using AspireApp.Web.Components.Pages;
+using Bunit;
+
+namespace AspireApp.Tests;
+
+public class CounterTests : TestContext
+{
+    [Fact]
+    public void CounterStartsAtZero()
+    {
+        // Arrange
+        var cut = RenderComponent<Counter>();
+
+        // Assert
+        cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 0</p>");
+    }
+
+    [Fact]
+    public void ClickingButtonIncrementsCounter()
+    {
+        // Arrange
+        var cut = RenderComponent<Counter>();
+
+        // Act
+        cut.Find("button").Click();
+
+        // Assert
+        cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 1</p>");
+    }
+}
