bf862acffda0804d0ad5faee1d4d64292df1f3b0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-28 09:24:01 +0530

    Swamy/25jan work (#10)
    * Code formatting
    
    * Update KeycloakHostingExtensions.cs
    
    * Update KeycloakHostingExtensions.cs
    
    * JWT Token Auth
    
    * Passing the token
    
    * Changes
    
    * Initial Version
    
    * Changes
    
    * Update Program.cs

---
+++ src/AspireStarterProjectWithSqlServer/AspireApp/AspireApp.AppHost/Program.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var password = builder.AddParameter("password", secret: true);
+
+var sql = builder.AddSqlServer(name: "sql", password, 1443)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataBindMount(source: @"D:\DataStores\DataVolume");
+
+var sqldb = sql.AddDatabase("sqldb", "ProductsDb");
+
+var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
+    .WithReference(sqldb);
+
+builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
