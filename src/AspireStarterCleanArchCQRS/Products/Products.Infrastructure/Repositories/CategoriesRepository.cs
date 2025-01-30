187cca1a5c768f91dcc0b3959de71bcf3078ddca
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-30 19:29:54 +0530

    Create CategoriesRepository.cs

---
+++ src/AspireStarterCleanArchCQRS/Products/Products.Infrastructure/Repositories/CategoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Products.Domain.Entities;
+using Products.Domain.Interfaces.Repositories;
+
+namespace Products.Infrastructure.Repositories;
+
+internal sealed class CategoriesRepository : ICategoriesRepository
+{
+    public Task<List<Category>> GetAllCategoriesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
