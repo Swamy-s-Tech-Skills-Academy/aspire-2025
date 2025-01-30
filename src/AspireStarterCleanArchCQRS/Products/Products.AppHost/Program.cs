e929f2faa3ce1e521bffe70d89e27891e0687a7c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-30 17:49:49 +0530

    Update Program.cs

--- src/AspireStarterCleanArchCQRS/Products/Products.AppHost/Program.cs
+++ src/AspireStarterCleanArchCQRS/Products/Products.AppHost/Program.cs
@@ -1,6 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Products_ApiService>("apiservice");
+var sqldb = builder.AddConnectionString("sqlDb");
+
+var apiService = builder.AddProject<Projects.Products_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
+    .WithReference(sqldb);
 
 builder.AddProject<Projects.Products_Web>("webfrontend")
     .WithExternalHttpEndpoints()
