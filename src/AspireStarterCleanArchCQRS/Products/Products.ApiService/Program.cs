df85dce07c134d4d87a72c781c26da3ec468caf2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-30 19:02:09 +0530

    Added Entities

--- src/AspireStarterCleanArchCQRS/Products/Products.ApiService/Program.cs
+++ src/AspireStarterCleanArchCQRS/Products/Products.ApiService/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Options;
-using Products.Infrastructure.Configuration;
 using Products.Infrastructure.Extensions;
 using Products.ServiceDefaults;
 
---
+++ src/AspireStarterCleanArchCQRS/Products/Products.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Domain.Entities;
+
+public class Category : Entity
+{
+    [MaxLength(255)]
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string Description { get; set; } = string.Empty;
+}
