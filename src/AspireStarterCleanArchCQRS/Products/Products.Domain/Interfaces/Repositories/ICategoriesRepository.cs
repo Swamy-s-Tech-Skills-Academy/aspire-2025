308824e77ca8f2b739d90a9fe87d855c5f493016
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-30 19:03:27 +0530

    Create ICategoriesRepository.cs

---
+++ src/AspireStarterCleanArchCQRS/Products/Products.Domain/Interfaces/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,8 @@
+﻿using Products.Domain.Entities;
+
+namespace Products.Domain.Interfaces.Repositories;
+
+public interface ICategoriesRepository
+{
+    Task<List<Category>> GetAllCategoriesAsync();
+}
